9229323c8213b3c49dda463b8f1e1ef10ff876cd
Olufemi Daniel <oladotun.olufemi@yahoo.com>
2020-10-09 00:05:52 +0100

    dashboard calculation update

--- js/dashboard.js
+++ js/dashboard.js
@@ -63,3 +63,85 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+
+
+// set endpoint and your API key
+apiKey = 'd884e934364c264fa566';
+
+//Set defaults
+
+defaultAmount = $('#sender_value').val();
+defaultFrom = $("#sender_currency").val();
+defaultTo = $("#receiver_currency").val();
+
+$("#sender_currency").change(function(){
+    var senderCurrency = $(this).children("option:selected").val();
+    var receiverCurrency = $("#receiver_currency").val();
+
+    var amount = $('#sender_value').val();
+
+    convert(senderCurrency,receiverCurrency,amount);
+});
+
+$("#receiver_currency").change(function(){
+    var receiverCurrency = $(this).children("option:selected").val();
+    var senderCurrency = $("#sender_currency").val();
+
+    var amount = $('#sender_value').val();
+
+    convert(senderCurrency,receiverCurrency,amount);
+});
+
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 2000;  //time in ms, 5 second for example
+var $input = $('.input');
+
+//on keyup, start the countdown
+$input.on('keyup', function (e) {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping(e), doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+    clearTimeout(typingTimer);
+});
+
+//user is "finished typing," do something
+function doneTyping (e) {
+    console.log(e.currentTarget.value);
+    console.log('hi');
+    var receiverCurrency = $("#receiver_currency").val();
+    var senderCurrency = $("#sender_currency").val();
+
+    var amount = $('#sender_value').val();
+
+    convert(senderCurrency,receiverCurrency,amount);
+}
+
+function convert(from, to , amount) {
+    console.log(from,to,amount)
+    var currVal = $("#receiver_value");
+    currVal.val("");
+
+    var query = from + "_" + to;
+
+    currVal.attr("placeholder", "Converting...");
+    $.getJSON(`https://free.currconv.com/api/v7/convert?q=${query}&compact=y&apiKey=${apiKey}&callback=?`,
+              function(data){
+        console.log(data);
+        try {
+            var currencyAmount = parseFloat(amount);
+            currVal.val(numeral(currencyAmount * data[query].val).format("0,0.00[0]"));
+        } catch (e) {
+            alert("Please enter a number in the Amount field.");
+        }
+
+        currVal.attr("placeholder", "Press Convert button");
+    });
+}
+
+convert(defaultFrom, defaultTo , defaultAmount);
+
+
